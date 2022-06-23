6bb0749bbcfee0638b575bf2c3efc9758ac52818
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-23 11:34:16 -0400

    Created font-awesome logo placeholder

---
+++ src/fontawesomeicons/CrossGreaterThan.js
@@ -0,0 +1,12 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCross, faGreaterThan} from '@fortawesome/free-solid-svg-icons';
+
+const CrossGreaterThan = () => {
+    return <div>
+        <FontAwesomeIcon icon={faCross} /> <FontAwesomeIcon icon={faGreaterThan} />
+    </div>
+}
+
+
+
+export default CrossGreaterThan;
