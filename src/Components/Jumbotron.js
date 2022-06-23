81bd0932034627f2597e8578a907d956791f2056
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-23 11:41:18 -0400

    Tweaked jumbotron again

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -4,18 +4,18 @@ import CrossGreaterThan from "../fontawesomeicons/CrossGreaterThan";
 const Jumbotron = () => {
     return (
         <header className="jumbotron jumbotron-fluid mb-0 primary-font">
-        <div className="container">
-            <div className="row">
-                <div className="col-4 col-sm-3 align-self-center title-logo">
-                    <CrossGreaterThan />
-                </div>
-                <div className="col">
-                <h1>Formed Church</h1>
-                <h4 className="d-none d-sm-block">Christ-centered, Community-focused</h4>
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-4 col-sm-3 align-self-center title-logo">
+                        <CrossGreaterThan />
+                    </div>
+                    <div className="col">
+                        <h1>Formed Church</h1>
+                        <h4 className="d-none d-sm-block">Christ-centered, Community-focused</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
     )
 };
 
