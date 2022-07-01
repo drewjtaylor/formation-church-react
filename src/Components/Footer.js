5b1ef12b123fa4e131fc62dea240e7f3629055a4
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-01 15:22:00 -0400

    Fixed footer social icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import {faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {Row, Col, Container,} from 'reactstrap';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     const footerStyle = "color: inherit;";
@@ -13,29 +14,31 @@ const Footer = () => {
                         <div className="col-4 col-sm-2 offset-1">
                             <h5>Links</h5>
                             <ul className="list-unstyled">
-                                <li><a href="index.html"> Home</a></li>
+                                <li><Link to="/"> Home</Link></li>
                                 <li><a href="/aboutus.html"> About</a></li>
-                                <li><a href="/contactus.html"> Contact</a></li>
+                                <li><a href="/contactpage.html"> Contact</a></li>
                             </ul>
                         </div>
                             <Col xs='6' sm='3' className='text-center'>
                                 <h5>Social</h5>
-                                <a className='btn btn-social-icon btn-instagram' href='http://instagram.com/'>
-                                    <FontAwesomeIcon icon={faInstagram} />
-                                </a>{' '}
-                                <a className='btn btn-social-icon btn-facebook' href='https://www.facebook.com/ourformationchurch/'>
-                                    <FontAwesomeIcon icon={faFacebook}/>
-                                </a>{' '}
-                                <a className='btn btn-social-icon btn-twitter' href='http://twitter.com/'>
-                                    <FontAwesomeIcon icon={faTwitter} />
-                                </a>{' '}
-                                <a className='btn btn-social-icon btn-google' href='http://youtube.com/'>
-                                    <FontAwesomeIcon icon={faYoutube} />
-                                </a>
+                                <div className="d-flex justify-content-around">
+                                    <a href='https://www.instagram.com/'>
+                                        <FontAwesomeIcon className='btn btn-social-icon btn-instagram' icon={faInstagram} />
+                                    </a>
+                                    <a href='https://www.facebook.com/ourformationchurch/'>
+                                        <FontAwesomeIcon className='btn btn-social-icon btn-facebook' icon={faFacebook}/>
+                                    </a>
+                                    <a href='http://twitter.com/'>
+                                        <FontAwesomeIcon className='btn btn-social-icon btn-twitter' icon={faTwitter} />
+                                    </a>
+                                    <a href='http://youtube.com/'>
+                                        <FontAwesomeIcon className='btn btn-social-icon btn-google' icon={faYoutube} />
+                                    </a>
+                                </div>
                             </Col>
                         <div className="col-md-4 text-center">
                             <a role="button" className="btn btn-link" href="tel:+17275551234"><FontAwesomeIcon icon={faPhone} /></a> 1-727-555-1234<br />
-                            <a role="button" className="btn btn-link" href="mailto:notarealemail@formedchurch.com"><FontAwesomeIcon icon={faEnvelope} />{' '}notarealemail@formedchurch.com </a>
+                            <a role="button" className="btn btn-link" href="mailto:notarealemail@formationchurch.com"><FontAwesomeIcon icon={faEnvelope} />{' '}notarealemail@formationchurch.com</a>
                         </div>
                     </Row>
                 </Container>
