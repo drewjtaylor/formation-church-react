f18ea554c29f6882e748db79a50303781b6cca52
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-01 18:15:31 -0400

    Removed unnecessary, warning-causing NavItems

--- src/Components/ChurchNavbar.js
+++ src/Components/ChurchNavbar.js
@@ -4,7 +4,7 @@ import {
     NavbarToggler,
     NavbarBrand,
     Nav,
-    NavItem,
+    // NavItem,
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
@@ -24,7 +24,7 @@ const ChurchNavbar = () => {
     return (
         <>
                 <Navbar color="light" light sticky='top' expand="md">
-                <NavbarBrand href="/"><img className='navbar-logo' src={logo} /></NavbarBrand>
+                <NavbarBrand href="/"><img className='navbar-logo' src={logo} alt='Formation Church'/></NavbarBrand>
                 <NavbarToggler onClick={() => toggle()} />
                     <Collapse isOpen={isOpen} navbar>
                         <Nav className="mr-auto" navbar>
@@ -33,34 +33,26 @@ const ChurchNavbar = () => {
                                 Who we are
                             </DropdownToggle>
                             <DropdownMenu end>
-                                <NavItem>
                                     <NavLink className='nav-link' to='WhyFormationChurch'>
                                         <DropdownItem>
                                             Why "Formation Church"
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink className="nav-link" to='LeadershipAndStaff'>
                                         <DropdownItem>
                                             Leadership and Staff
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink className="nav-link" to='WhatWeBelieve'>
                                         <DropdownItem>
                                             What We Believe
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink className="nav-link" to='OurMission'>
                                         <DropdownItem>
                                             Our Mission
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
                         <UncontrolledDropdown nav inNavbar>
@@ -68,27 +60,21 @@ const ChurchNavbar = () => {
                             Let's Connect
                             </DropdownToggle>
                             <DropdownMenu end>
-                                <NavItem>
                                     <NavLink className="nav-link" to='events'>
                                         <DropdownItem>
                                             Events
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                <NavItem>
                                     <NavLink className="nav-link" to='ChurchGatherings'>
                                         <DropdownItem>
                                             Church Gatherings
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
                                     <NavLink className="nav-link" to='ContactPage'>
                                         <DropdownItem>
                                             Contact Us
                                         </DropdownItem>
                                     </NavLink>
-                                </NavItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
                         <UncontrolledDropdown nav inNavbar>
@@ -100,10 +86,10 @@ const ChurchNavbar = () => {
                                     <a href="https://formed-church.netlify.app/That-Man-Called-Jesus.pdf">Book Club Guide</a>
                                 </DropdownItem>
                                 <DropdownItem>
-                                    <a href="https://www.biblegateway.com/" className='unstyled-link' target='_blank'>Read the Bible Online</a>
+                                    <a href="https://www.biblegateway.com/" className='unstyled-link' target='_blank' rel="noreferrer">Read the Bible Online</a>
                                 </DropdownItem>
                                 <DropdownItem>
-                                    <a href="https://assistcx.org/" className='unstyled-link' target='_blank'><strong>Assist</strong> Church Expansion Ministry</a>
+                                    <a href="https://assistcx.org/" className='unstyled-link' target='_blank' rel="noreferrer"><strong>Assist</strong> Church Expansion Ministry</a>
                                 </DropdownItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
