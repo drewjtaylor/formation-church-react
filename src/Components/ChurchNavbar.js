084ef36d66129de9e357f484d526f6b9b5f4439f
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-06-30 22:40:02 -0400

    Navbar links now go through router

--- src/Components/ChurchNavbar.js
+++ src/Components/ChurchNavbar.js
@@ -5,7 +5,6 @@ import {
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink,
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
@@ -13,6 +12,7 @@ import {
 } from 'reactstrap';
 import logo from '../img/FLogo.jpg';
 import {useState} from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 const ChurchNavbar = () => {
@@ -27,24 +27,40 @@ const ChurchNavbar = () => {
                 <NavbarBrand href="/"><img className='navbar-logo' src={logo} /></NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                     <Collapse isOpen={isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
+                        <Nav className="mr-auto" navbar>
                         <UncontrolledDropdown nav inNavbar>
                             <DropdownToggle nav caret>
                             Who we are
                             </DropdownToggle>
                             <DropdownMenu end>
-                                <DropdownItem>
-                                    Why "Formation Church?"
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Leadership and Staff
-                                </DropdownItem>
-                                <DropdownItem>
-                                    What We Believe
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Our Mission
-                                </DropdownItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='WhyFormationChurch'>
+                                        <DropdownItem>
+                                            Why "Formation Church?"
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='LeadershipAndStaff'>
+                                        <DropdownItem>
+                                            Leadership and Staff
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='WhatWeBelieve'>
+                                        <DropdownItem>
+                                            What We Believe
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='OurMission'>
+                                        <DropdownItem>
+                                            Our Mission
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
                         <UncontrolledDropdown nav inNavbar>
@@ -52,27 +68,35 @@ const ChurchNavbar = () => {
                             Let's Connect
                             </DropdownToggle>
                             <DropdownMenu end>
-                                <DropdownItem>
-                                    Church Gatherings
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Contact Us
-                                </DropdownItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='ChurchGatherings'>
+                                        <DropdownItem>
+                                            Church Gatherings
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='ContactPage'>
+                                        <DropdownItem>
+                                            Contact Us
+                                        </DropdownItem>
+                                    </NavLink>
+                                </NavItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
                         <UncontrolledDropdown nav inNavbar>
                             <DropdownToggle nav caret>
-                            Resources
+                            External Resources
                             </DropdownToggle>
                             <DropdownMenu end>
                                 <DropdownItem>
                                     Book Club Guide
                                 </DropdownItem>
                                 <DropdownItem>
-                                    Read the Bible Online
+                                    <a href="https://www.biblegateway.com/" className='unstyled-link'>Read the Bible Online</a>
                                 </DropdownItem>
                                 <DropdownItem>
-                                    <strong>Assist</strong> Church Expansion Ministry
+                                    <a href="https://assistcx.org/" className='unstyled-link'><strong>Assist</strong> Church Expansion Ministry</a>
                                 </DropdownItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
