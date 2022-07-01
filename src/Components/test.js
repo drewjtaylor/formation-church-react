2fe8a4a430832cbffcf91342513d9fa175d6e590
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-01 15:24:35 -0400

    font-awesome component practice

---
+++ src/Components/test.js
@@ -0,0 +1,15 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import {faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons';
+
+// const Test = () => {
+//     return (
+//         <>
+//             <h3>This is the test section</h3>
+//             <i class="fas-solid fas-ball-pile"></i>
+//             <h3>End of test section</h3>
+//         </>
+//     )
+// };
+
+// export default Test;
