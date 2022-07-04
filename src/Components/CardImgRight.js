b69b56d39a8614f7b14d062bc6d7c75ad41be8ca
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-03 21:30:08 -0400

    Made basic informational cards

---
+++ src/Components/CardImgRight.js
@@ -0,0 +1,26 @@
+import {Container, Row, Col} from 'reactstrap';
+
+const CardImgRight = ({cardInfo}) => {
+    const {imageSrc, imageAlt, headerContent, textContent} = cardInfo;
+    
+    if (imageSrc) {
+        return <Container className='pt-5'>
+            <Row className='align-items-center'>
+                <Col xs='9'>
+                    <Row><h3 className='style-font'>{headerContent}</h3></Row>
+                    <Row><h4 className='style-font'>{textContent}</h4></Row>
+                </Col>
+                <Col xs='3'>
+                    <img src={imageSrc} alt={imageAlt} className='img-fluid'/>
+                </Col>
+            </Row>
+        </Container>
+    }
+    
+    return <Container className='pt-5'>
+                <Row><h3 className='style-font text-center'>{headerContent}</h3></Row>
+                <Row><h4 className='style-font text-center'>{textContent}</h4></Row>
+    </Container>
+};
+
+export default CardImgRight;
