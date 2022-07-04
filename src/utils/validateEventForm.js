a4119bcbe08da62c91a002e73ea5b14e625e5d5c
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-04 12:01:41 -0400

    just using date, instead of date and time

--- src/utils/validateEventForm.js
+++ src/utils/validateEventForm.js
@@ -1,14 +1,11 @@
 export const validateEventForm = (values) => {
-    const {date, title, time} = values;
+    const {date, title} = values;
     const errors = {};
     
     if (!date) {
         errors.date = 'Required';
     }
 
-    if (!time) {
-        errors.time = 'Required';
-    }
 
     if (!title) {
         errors.title = 'Required';
