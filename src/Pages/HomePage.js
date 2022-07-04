20e488d78946cbd24e0b47207ef6c821d0bee738
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-03 21:30:18 -0400

    Started filling in homepage with basic cards

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,37 @@
 import MainCarousel from "../Components/MainCarousel";
+import CardImgRight from "../Components/CardImgRight";
+import CardImgLeft from "../Components/CardImgLeft";
+import crossGreaterThan from '../img/cross-greater-than.png'
+import colorTampa from '../img/assist-tampa-600x401.jpg';
 
 const HomePage = () => {
+    const firstCard = {
+        imageSrc: crossGreaterThan,
+        imageAlt: 'The cross is greater',
+        headerContent: 'Christ-centered in what we do.',
+        textContent: 'Community-focused in how we do it.',
+    }
+
+    const secondCard = {
+        imageSrc: colorTampa,
+        imageAlt: 'tampa bay',
+        headerContent: '',
+        textContent: 'If you live in the St. Petersburg/Tampa Bay area, (or you\'re just visiting), we would love for you to come join us.',
+    }
+
+    const thirdCard = {
+        imageSrc: '',
+        imageAlt: '',
+        headerContent: '',
+        textContent: '',
+    }
+    
     return (
         <>
             <MainCarousel />
+            <CardImgLeft cardInfo={firstCard}/>
+            <CardImgRight cardInfo={secondCard} />
+            <CardImgLeft cardInfo={thirdCard} />
         </>
     )
 };
