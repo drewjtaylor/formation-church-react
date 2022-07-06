8f6b656f3ff6830282f646fba65038e835953d95
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-06 18:45:00 -0400

    styled events page a little

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -7,17 +7,15 @@ import demoEvents from "../demoData/demoEvents";
 const Events = () => {
     const [eventList, setEventList] = useState(demoEvents);
 
-    
 
     return (
         <>
             <Container>
-            <h1>Welcome to the Church events page</h1>
-            <p>Please note our upcoming events on the left.</p>
-            <p>If you would like to add an event, please enter the information in the form on the right</p>
+            <h1 className="text-center">Welcome to the Church events page</h1>
+            <h5 className="text-center mb-xxl-5">If you would like to add an event, please enter the information in the form on the right</h5>
                 <Row>
                     <Col>
-                        <div className='overflow-auto'>
+                        <div>
                             <EventList events={eventList}/>
                         </div>
                     </Col>
