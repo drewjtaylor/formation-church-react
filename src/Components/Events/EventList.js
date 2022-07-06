229c14d1f19cfd8f74c64abd71ee36c533c4fa45
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-06 18:45:09 -0400

    Only future events render

--- src/Components/Events/EventList.js
+++ src/Components/Events/EventList.js
@@ -1,24 +1,32 @@
 import {Card, CardTitle, CardSubtitle, CardBody, CardText} from 'reactstrap';
-import { SelectAllEvents } from './eventSlice';
+import { eventsReducer, SelectAllEvents } from './eventSlice';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 
 const EventList = () => {  
     const allEvents = useSelector(SelectAllEvents);
+    const sortedEvents = [...allEvents]; //Makes a copy of all events so we're not mutating it with "sort()"
+
+    sortedEvents.sort((a, b) => Number(a.date)-Number(b.date));  // Now we have "sortedEvents" to use which are in order
+
+
+
 
     return (
         allEvents.map((event) => {
             const displayDate = event.date.toDateString();
-            const stringDisplayTime = event.date.toLocaleTimeString();
-            const displayTime = `${stringDisplayTime.slice(0, 5)} ${stringDisplayTime.slice(stringDisplayTime-2)}`
+            const displayTime = event.date.toLocaleTimeString('en-us', {hour: '2-digit', minute:'2-digit'});
+            if ((new Date()) < event.date) {  // Only renders if the date is in the future
+                return <Card key={event.id}>
+                    <CardBody>
+                        <CardTitle className='eventcardTitle bg-primary text-light display-6'>{event.title}</CardTitle>
+                        <CardSubtitle className='eventcardSubtitle bg-primary text-light display-'>{displayDate}, {displayTime}</CardSubtitle>
+                        <CardText className='eventcardText bg-light'>{event.description}</CardText>
+                    </CardBody>
+                </Card>
+            }
 
-            return <Card key={event.id}>
-                <CardBody>
-                    <CardTitle className='eventcardTitle bg-primary text-light display-6'>{event.title}</CardTitle>
-                    <CardSubtitle className='eventcardSubtitle bg-primary text-light display-'>{displayDate}, {displayTime}</CardSubtitle>
-                    <CardText className='eventcardText bg-light'>{event.description}</CardText>
-                </CardBody>
-            </Card>
         })
     )
 };
